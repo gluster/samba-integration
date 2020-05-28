cf1cd270fdcd2803ff4e7a74383b866ce85fab7b
Sachin Prabhu <sprabhu@redhat.com>
2020-05-28 14:37:13 +0100

    Add new options to the smbtorture command
    This matches what is set by the selftest infrastructure in samba.
    
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>

--- testcases/smbtorture-test/smbtorture-test.py
+++ testcases/smbtorture-test/smbtorture-test.py
@@ -7,11 +7,14 @@ import sys, os
 import yaml
 
 smbtorture_exec = "/bin/smbtorture"
+
 output = testhelper.get_tmp_file("/tmp")
 
 def smbtorture(mount_params, test, output):
-    cmd = "%s --target=samba3 --user=%s%%%s //%s/%s %s >%s 2>&1" % (
+    smbtorture_options_str = "--option=torture:progress=no --option=torture:sharedelay=100000 --option=torture:writetimeupdatedelay=500000"
+    smbtorture_cmd = "%s %s --format=subunit --target=samba3 --user=%s%%%s //%s/%s %s >%s 2>&1" % (
                                             smbtorture_exec,
+                                            smbtorture_options_str,
                                             mount_params["username"],
                                             mount_params["password"],
                                             mount_params["host"],
@@ -19,6 +22,8 @@ def smbtorture(mount_params, test, output):
                                             test,
                                             output
                                          )
+
+    cmd = "%s" % (smbtorture_cmd)
     ret = os.system(cmd)
     return ret == 0
 
