34ecec93ffbdcfdb6b9342b32db9318f65df1b75
Sachin Prabhu <sprabhu@redhat.com>
2020-07-27 21:05:55 +0100

    Fix mount test to support multiple shares
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>

--- testcases/mount-test/mount-test.py
+++ testcases/mount-test/mount-test.py
@@ -18,16 +18,24 @@ tmp_root = testhelper.get_tmp_root()
 mount_point = testhelper.get_tmp_mount_point(tmp_root)
 mount_params = testhelper.get_default_mount_params(test_info)
 
-
 flag_share_mounted = 0
 flag_file_created = 0
 try:
-    testhelper.cifs_mount(mount_params, mount_point)
-    flag_share_mounted = 1
-    test_file = testhelper.get_tmp_file(mount_point)
-    flag_file_created = 1
-    with open(test_file, 'w') as f:
-        f.write(test_string)
+    print("\n")
+    for i in range(testhelper.get_num_shares(test_info)):
+        mount_params["share"] = testhelper.get_share(test_info, i)
+        print("Testing //%s/%s" % (mount_params["host"],
+                                         mount_params["share"]))
+        testhelper.cifs_mount(mount_params, mount_point)
+        flag_share_mounted = 1
+        test_file = testhelper.get_tmp_file(mount_point)
+        flag_file_created = 1
+        with open(test_file, 'w') as f:
+            f.write(test_string)
+        os.unlink(test_file)
+        flag_file_created = 0
+        testhelper.cifs_umount(mount_point)
+        flag_share_mounted = 0
 except:
     print("Error while executing test")
     raise
