b3f08f9c6dc43702458592502bee4addec7c714f
Sachin Prabhu <sprabhu@redhat.com>
2020-07-28 09:43:10 +0100

    Modify consistency check to test through various shares
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>

--- testcases/consistency-test/consistency-check.py
+++ testcases/consistency-test/consistency-check.py
@@ -25,30 +25,42 @@ test_info = testhelper.read_yaml(test_info_file)
 tmp_root = testhelper.get_tmp_root()
 mount_point = testhelper.get_tmp_mount_point(tmp_root)
 mount_params = testhelper.get_default_mount_params(test_info)
-share = mount_params["share"]
 
 # First open the default share and write a file with contents
 # set to the test_string
 flag_share_mounted = 0
 flag_file_created = 0
 try:
-    testhelper.cifs_mount(mount_params, mount_point)
-    flag_share_mounted = 1
-    test_file = testhelper.get_tmp_file(mount_point)
-    flag_file_created = 1
-    with open(test_file, 'w') as f:
-        f.write(test_string)
-    testhelper.cifs_umount(mount_point)
-    flag_share_mounted = 0
-
-    for i in range(1, testhelper.get_total_mount_parameter_combinations(test_info)):
-        mount_params = testhelper.get_mount_parameter(test_info, share, i)
+    print("\n")
+    for sharenum in range(testhelper.get_num_shares(test_info)):
+        share = testhelper.get_share(test_info, sharenum)
+        mount_params["share"] = share
+        print("Testing %s" % (share))
         testhelper.cifs_mount(mount_params, mount_point)
         flag_share_mounted = 1
-        with open(test_file, 'r') as f:
-            assert file_content_check(f, test_string), "File content does not match"
+        test_file = testhelper.get_tmp_file(mount_point)
+        flag_file_created = 1
+        with open(test_file, 'w') as f:
+            f.write(test_string)
         testhelper.cifs_umount(mount_point)
         flag_share_mounted = 0
+
+        for i in range(1, testhelper.get_total_mount_parameter_combinations(test_info)):
+            mount_params = testhelper.get_mount_parameter(test_info, share, i)
+            testhelper.cifs_mount(mount_params, mount_point)
+            flag_share_mounted = 1
+            with open(test_file, 'r') as f:
+                assert file_content_check(f, test_string), "File content does not match"
+            testhelper.cifs_umount(mount_point)
+            flag_share_mounted = 0
+
+        if (flag_file_created == 1):
+            testhelper.cifs_mount(mount_params, mount_point)
+            flag_share_mounted = 1
+            os.unlink(test_file)
+            flag_file_created = 0
+            testhelper.cifs_umount(mount_point)
+            flag_share_mounted = 0
 
 except:
     print("Error while executing test")
